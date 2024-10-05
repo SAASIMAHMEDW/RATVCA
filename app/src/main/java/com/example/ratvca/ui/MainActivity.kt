ceca67cdb3b4a63731fa1a8b01c3e14362734473
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-10-06 03:24:35 +0530

    Real Time Translation Video Calling App

---
+++ app/src/main/java/com/example/ratvca/ui/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.example.ratvca.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ratvca.adapters.MainRecyclerViewAdapter
+import com.example.ratvca.databinding.ActivityMainBinding
+import com.example.ratvca.repository.MainRepository
+import com.example.ratvca.service.MainService
+import com.example.ratvca.service.MainServiceRepository
+import com.example.ratvca.utils.DataModel
+import com.example.ratvca.utils.DataModelType
+import com.example.ratvca.utils.getCameraAndMicPermission
+
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), MainRecyclerViewAdapter.Listener, MainService.Listener {
+    private val TAG = "MainActivity"
+
+    private lateinit var views: ActivityMainBinding
+    private var username: String? = null
+
+    @Inject
+    lateinit var mainRepository: MainRepository
+    @Inject
+    lateinit var mainServiceRepository: MainServiceRepository
+    private var mainAdapter: MainRecyclerViewAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        init()
+    }
+
+    private fun init() {
+        username = intent.getStringExtra("username")
+        if (username == null) finish()
+        //1. observe other users status
+        subscribeObservers()
+        //2. start foreground service to listen negotiations and calls.
+        startMyService()
+    }
+
+    private fun subscribeObservers() {
+        setupRecyclerView()
+        MainService.listener = this
+        mainRepository.observeUsersStatus {
+            Log.d(TAG, "subscribeObservers: $it")
+            mainAdapter?.updateList(it)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        mainAdapter = MainRecyclerViewAdapter(this)
+        val layoutManager = LinearLayoutManager(this)
+        views.mainRecyclerView.apply {
+            setLayoutManager(layoutManager)
+            adapter = mainAdapter
+        }
+    }
+
+    private fun startMyService() {
+        mainServiceRepository.startService(username!!)
+    }
+
+    override fun onVideoCallClicked(username: String) {
+        //check if permission of mic and camera is taken
+        getCameraAndMicPermission {
+            mainRepository.sendConnectionRequest(username, true) {
+                if (it){
+                    //we have to start video call
+                    //we wanna create an intent to move to call activity
+                    startActivity(Intent(this,CallActivity::class.java).apply {
+                        putExtra("target",username)
+                        putExtra("isVideoCall",true)
+                        putExtra("isCaller",true)
+                    })
+
+                }
+            }
+
+        }
+    }
+
+    override fun onAudioCallClicked(username: String) {
+        getCameraAndMicPermission {
+            mainRepository.sendConnectionRequest(username, false) {
+                if (it){
+                    //we have to start audio call
+                    //we wanna create an intent to move to call activity
+                    startActivity(Intent(this,CallActivity::class.java).apply {
+                        putExtra("target",username)
+                        putExtra("isVideoCall",false)
+                        putExtra("isCaller",true)
+                    })
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mainServiceRepository.stopService()
+    }
+
+    override fun onCallReceived(model: DataModel) {
+        runOnUiThread {
+            views.apply {
+                val isVideoCall = model.type == DataModelType.StartVideoCall
+                val isVideoCallText = if (isVideoCall) "Video" else "Audio"
+                incomingCallTitleTv.text = "${model.sender} is $isVideoCallText Calling you"
+                incomingCallLayout.isVisible = true
+                acceptButton.setOnClickListener {
+                    getCameraAndMicPermission {
+                        incomingCallLayout.isVisible = false
+                        //create an intent to go to video call activity
+                        startActivity(Intent(this@MainActivity,CallActivity::class.java).apply {
+                            putExtra("target",model.sender)
+                            putExtra("isVideoCall",isVideoCall)
+                            putExtra("isCaller",false)
+                        })
+                    }
+                }
+                declineButton.setOnClickListener {
+                    incomingCallLayout.isVisible = false
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/ratvca/utils/UserStatus.kt
@@ -0,0 +1,5 @@
+package com.example.ratvca.utils
+
+enum class UserStatus {
+    ONLINE,OFFLINE,IN_CALL
+}
